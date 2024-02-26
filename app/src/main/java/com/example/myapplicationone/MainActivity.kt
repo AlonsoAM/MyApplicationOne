e62882fb418a7759590f347c943745321609e6f5
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-02-26 12:22:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationone/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.example.myapplicationone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplicationone.ui.theme.MyApplicationOneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationOneTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Content()
+                }
+            }
+        }
+    }
+}
+
+/*
+* Modificadores
+* Hay 4 tipos de modificadores:
+* 1. De posicionamiento = width, height, padding, margin, offset, align, etc.
+* 2. De funcionalidad = clickable, focusable, draggable, etc.
+* 3. De apariencia = background, border, shadow, clip, etc.
+* 4. De escucha = onClik, onLongClick, onDrag, etc.
+* */
+
+val items = listOf(Color.Red, Color.Yellow, Color.Black, Color.DarkGray, Color.Magenta, Color.Cyan)
+val nombre = "Me Gusta"
+
+
+@Preview(showBackground = true)
+@Composable
+fun Content() {
+    var likes by remember { mutableStateOf(0) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+            .padding(horizontal = 10.dp)
+    ) {
+        Texto("Bienvenido")
+        Space()
+        Texto("Jetpack")
+        Space()
+        Texto("Compose")
+        Space()
+        LazyRow(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            items.forEach {
+                item {
+                    Circulo(it)
+                    Spacer(modifier = Modifier.width(16.dp))
+                }
+            }
+        }
+        Space()
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = {
+                likes++
+            }) {
+                Text(text = nombre)
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Resultado(likes = likes)
+        }
+    }
+}
+
+@Composable
+fun Texto(texto: String) {
+    Text(
+        text = texto,
+        color = Color.Red,
+        fontWeight = FontWeight.Bold,
+        fontSize = 35.sp,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(Color.Yellow)
+            .padding(horizontal = 16.dp)
+            .fillMaxWidth()
+            .clickable {
+                println("Hola Jetpack!")
+            }
+    )
+}
+
+@Composable
+fun Resultado(likes: Int) {
+    Text(text = likes.toString(), fontWeight = FontWeight.Bold, fontSize = 50.sp)
+}
+
+@Composable
+fun Circulo(color: Color) {
+    Box(
+        modifier = Modifier
+            .size(70.dp)
+            .background(color, CircleShape)
+    )
+}
+
+@Composable
+fun Space() {
+    Spacer(modifier = Modifier.height(16.dp))
+}
